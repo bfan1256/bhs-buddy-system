84219580d27d982e05f0f4fc92c339d3268f4a3d
Brandon Fan <brandonfan1256@gmail.com>
2018-09-12 22:32:41 -0400

    ignore environment files

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyD5Fo52N4feWibWjaWCrJoyl--_0KY8vr8',
+    authDomain: 'bhs-buddy-system.firebaseapp.com',
+    databaseURL: 'https://bhs-buddy-system.firebaseio.com',
+    projectId: 'bhs-buddy-system',
+    storageBucket: 'bhs-buddy-system.appspot.com',
+    messagingSenderId: '334528870697'
+  }
 };
+
